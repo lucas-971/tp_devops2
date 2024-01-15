866b896e875ed654babd80705ee2c836f46ee399
lucas-971 <70963689+lucas-971@users.noreply.github.com>
2024-01-15 15:29:44 +0100

    tt

--- random_picking.py
+++ random_picking.py
@@ -2,10 +2,13 @@ import math
 
 def proba_extraction(k, nb_rouge, nb_blanche, nb_noire):
 
+
     total_boules = nb_rouge + nb_blanche + nb_noire
 
     total_combinaisons = math.comb(total_boules, k)
-    combinaisons_une_de_chaque = math.comb(nb_rouge, 1) * math.comb(nb_blanche, 1) * math.comb(nb_noire, 1)
+    combinaisons_une_de_chaque = (
+        math.comb(nb_rouge, 1) * math.comb(nb_blanche, 1) * math.comb(nb_noire, 1)
+    )
 
     proba = combinaisons_une_de_chaque / total_combinaisons
 
@@ -17,4 +20,6 @@ nb_blanche = 1
 nb_noire = 1
 
 resultat = proba_extraction(k, nb_rouge, nb_blanche, nb_noire)
-print(f"La probabilité d'obtenir exactement une boule de chaque couleur est : {resultat}")
+print(
+    f"La probabilité d'obtenir exactement une boule de chaque couleur est : {resultat}"
+    )
