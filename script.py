d68b308d301ebe3ea493d9284144227c9ab23078
lucas-971 <70963689+lucas-971@users.noreply.github.com>
2024-01-15 15:09:59 +0100

    test

--- script.py
+++
@@ -1,5 +0,0 @@
-for n in range(1,10):
-    print("--------------------------------------")
-    print("la table de multiplication de  : ", n," est :")
-    for i in range(1,10):
-        print(i , " x ", n, " = ",i*n)
--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-a = 1
-b = os.environ['VARIABLE']
-
-if a == b:
-    print("A est pareil que B.")
-else:
-    print("A est différent de B.")
