6c9b67c0393e0658c8f8d85d5d657563bb707407
lucas-971 <70963689+lucas-971@users.noreply.github.com>
2024-01-15 13:41:17 +0100

    first_commit

---
+++ script.py
@@ -0,0 +1,5 @@
+for n in range(1,10):
+    print("--------------------------------------")
+    print("la table de multiplication de  : ", n," est :")
+    for i in range(1,10):
+        print(i , " x ", n, " = ",i*n)
