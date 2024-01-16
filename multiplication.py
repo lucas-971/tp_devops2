a92f4c162b43d9c4aec16d93dd07e2956537664f
lucas-971 <70963689+lucas-971@users.noreply.github.com>
2024-01-16 11:29:54 +0100

    dst

--- multiplication.py
+++ multiplication.py
@@ -1,4 +1,8 @@
-import os
+import os, numpy
 
 c = int(os.environ["A"]) + int(os.environ["B"])
+d =  numpy.log(int(os.environ["A"]) * int(os.environ["B"]))
+e =  numpy.log(int(os.environ["A"])) + numpy.log(int(os.environ["B"]))
 print(c)
+print(d)
+print(e)
